9bfd6ebb56737b6da00cc2ec83db2a0c0a187106
陆远 <qq02_008@163.com>
2017-11-26 23:30:27 +0800

    初始化

---
+++ EarthIsm/EarthIsm.cpp
@@ -0,0 +1,9 @@
+//
+//  EarthIsm.cpp
+//  EarthIsm
+//
+//  Created by 陆远 on 2017/11/26.
+//  Copyright © 2017年 EarthIsm. All rights reserved.
+//
+
+#include "EarthIsm.h"
---
+++ EarthIsm/EarthIsm.h
@@ -0,0 +1,14 @@
+//
+//  EarthIsm.hpp
+//  EarthIsm
+//
+//  Created by 陆远 on 2017/11/26.
+//  Copyright © 2017年 EarthIsm. All rights reserved.
+//
+
+#ifndef EarthIsm_hpp
+#define EarthIsm_hpp
+
+#include <stdio.h>
+
+#endif /* EarthIsm_hpp */
